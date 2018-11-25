5001c9a8250c4cfe078c477e3ede23cfcd0fc611
mohamedabdelhakem1 <mohamedabdelhakem99@yahoo.com>
2018-11-26 02:13:54 +0400

    sds

--- src/eg/edu/alexu/csd/oop/db/cs43/MyDatabase.java
+++ src/eg/edu/alexu/csd/oop/db/cs43/MyDatabase.java
@@ -14,6 +14,8 @@ public class MyDatabase implements Database {
 	public MyDatabase() {
 		commandsParser = new CommandsParser();
 	}
+	
+	
 
 	@Override
 	public String createDatabase(String databaseName, boolean dropIfExists) {
